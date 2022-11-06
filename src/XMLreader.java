d405db108b1ea19fbf6d4e8d00f4d2d939b4ebcb
Евгений <evg.krivorotov@mail.ru>
2022-11-06 21:37:42 +0300

    added xml creating

--- src/XMLreader.java
+++ src/XMLreader.java
@@ -1,30 +1,44 @@
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.*;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XMLreader {
 
     public XMLreader() throws ParserConfigurationException, TransformerException {
+        String filepath = "test.xml";
+        File xmlFile = new File(filepath);
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        Document doc = factory.newDocumentBuilder().newDocument();
-        Element root = doc.createElement("root");
-        root.setAttribute("xmlns", "xmltesting");
-        doc.appendChild(root);
+        DocumentBuilder builder;
+        try {
+            builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+            System.out.println("Корневой элемент: " + doc.getDocumentElement().getNodeName());
+            // получаем узлы с именем Language
+            // теперь XML полностью загружен в память
+            // в виде объекта Document
+            NodeList nodeList = doc.getElementsByTagName("String");
 
-        Element item1 = doc.createElement("String");
-        item1.setAttribute("String", "just testing here\n" +
-                "some features nearly added\n" +
-                "fffafafqaf");
-        root.appendChild(item1);
-        File file = new File("test.xml");
+            String Result="";
+            for (int i = 0; i < nodeList.getLength(); i++) {
 
-        Transformer transformer = TransformerFactory.newInstance().newTransformer();
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.transform(new DOMSource(doc), new StreamResult(file));
+            }
+
+            // печатаем в консоль информацию по каждому объекту Language
+            System.out.println(Result);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+
+
     }
 }
